039b70bc6f2b6de7ffaca7d09ebaace8fd601e77
JonathanC13 <16930415+JonathanC13@users.noreply.github.com>
2026-02-04 17:19:14 -0500

    shuffling

--- Camera-source/setup.py
+++ Camera-source/setup.py
@@ -1,19 +1,14 @@
-import logging
+from dotenv import load_dotenv
 import os
-import CreateConfigSettings
+import configparser
+import logging
 
-# setup Folders and logger
-def setup():
+def verifyFolders(folders):
     currPath = os.getcwd()    
     
     # project's folders
     print("Folders: checking...")
-    folders = {
-            'imageOutputFolder' : 'imageOutput',
-            'recordedFolder' : 'recorded',
-            'loggingFolder' : 'logging',
-            'configFolder' : 'config'
-        }
+    
     for k, folder_name in folders.items():
         folder_path = currPath + "/" + folder_name
         if os.path.isdir(folder_path):
@@ -27,12 +22,13 @@ def setup():
                 print(f"ERR: The folder '{folder_path}' could not be created. {e}")
                 print("ERR: Could not initialize program. Quitting...")
                 return False
-    print("Folders: GOOD")
+    print("Folders: complete.")
     
-    logInfo = {
-            'loggerName' : "my_camera_logger",
-            'loggerFile' : "camera_application.log"
-        }
+    return True
+
+
+def setLogger(logInfo):
+    currPath = os.getcwd()
     
     # logger
     loggerName = logInfo['loggerName']
@@ -50,11 +46,156 @@ def setup():
     if not logger.handlers: # Prevent adding multiple handlers if the logger is re-configured
         logger.addHandler(file_handler)
         
-    logger.info("Logger started.")
+    return logInfo['loggerName'] in logging.Logger.manager.loggerDict
+
+
+def verifyEnv(envKeys, logInfo):
+    currPath = os.getcwd()
     
+    print(f".env: checking...")
+    envPath = currPath + "/" + ".env"
     
-    configStatus = CreateConfigSettings.main(folders, logInfo)
-    if (configStatus == False):
+    loggerName = logInfo['loggerName']
+    logger = logging.getLogger(loggerName)
+    
+    # check if .env file exists
+    try:
+        with open(envPath, 'x') as file:
+            for key in envKeys:
+                set_key(envPath, key, "")
+           
+        logger.info(f".env file created successfully... GOOD")
+    except FileExistsError:
+        logger.info(f".env file exists... GOOD")
+    except Exception as e:
+        logger.error(f"ERR: Error creating .env file occurred: {e}")
+        print(f"Error checking .env file... see log file.")
+        return False
+        
+    load_dotenv(override=True) # This loads the variables from .env
+    
+    for key in envKeys:
+        val = os.getenv(key)
+        if (val is None):
+            # create key
+            try:
+                with open(envPath, 'a') as file:
+                    set_key(envPath, key, "")
+            except Exception as e:
+                logger.error(f"ERR: Error writing to .env file occurred: {e}")
+                print(f"Error writing to .env file... see log file.")
+                return False
+                
+        val = os.getenv(key)
+        if (val == ""):
+            while (val == ""):
+                # prompt user for value
+                val = input(f"Value for .env key is empty. Enter value:")
+            
+            set_key(envPath, key, val)
+    
+    print(f".env: complete.")
+    return True
+
+def validateConfigKeys(config_dict, configResources):
+    for key in configResources:
+        if key not in config_dict:
+            config_dict[key] = configResources[key]
+        else:
+            validateConfigKeys(config_dict[key], configResources[key])
+        
+
+def checkConfigFileExists(configInfo, folders, logInfo):
+    currPath = os.getcwd()
+    config_file_path = os.path.join(currPath, folders['configFolder'], configInfo['configFileName'])
+    
+    print('Config file: checking...')
+    
+    config = configparser.ConfigParser()
+        
+    configSettings = {
+            "comment" : "If application has erros related to this config file, either manually configure or delete this file then re-run.",
+            "cameraName" : "this camera",
+            "recordedPath" : currPath + "/recorded"
+            #"api_uploadVideoURL" : "http://192.168.1.2:5000/api/v1/uploadVideo"
+        }
+    
+    configResources = {
+        'SETTINGS': configSettings,
+        'FOLDERS': folders,
+        'LOG_INFO': logInfo,
+        'CONFIG_INFO': configInfo
+        }
+    
+    config.read_dict(configResources)
+    
+    loggerName = logInfo['loggerName']
+    logger = logging.getLogger(loggerName)
+    
+    try:
+        with open(config_file_path, 'x') as configfile:
+            config.write(configfile)
+        logger.info(f"Config file: Configuration file '{config_file_path}' created successfully.")
+    except FileExistsError:
+        logger.info(f"Config file exists... GOOD... If application has config errors, either manually configure or delete {config_file_path} then re-run.")
+        
+        config.read('config.ini')
+        # check keys
+        config_dict = {}
+        for section in config.sections():
+            config_dict[section] = dict(config.items(section))
+        
+        validateConfigKeys(config_dict, configResources)
+        
+        configVal = configparser.ConfigParser()
+        configVal.read_dict(configResources)
+
+        try:
+            with open(config_file_path, 'w') as configfileVal:
+                configVal.write(configfileVal)
+        except Exception as e:
+            # Catch any other unexpected errors
+            print(f"ERR: Config file: Error writing to config file: {e}")
+            logger.error(f"ERR: Config file: Error writing to config file: {e}")
+            return False
+        
+    except Exception as e:
+        logger.error(f"ERR: Config file: Error creating configuration file '{config_file_path}': {e}")
+        return False
+    
+    print('Config file: complete.')
+    retrun True
+
+# setup Folders and logger
+def setup():
+    
+    folders = {
+            'imageOutputFolder' : 'imageOutput',
+            'recordedFolder' : 'recorded',
+            'loggingFolder' : 'logging',
+            'configFolder' : 'config'
+        }
+    if verifyFolders(folders) == False:
+        print('Necessary folders could not be created... Quitting')
+        return False
+    
+    logInfo = {
+            'loggerName' : "my_camera_logger",
+            'loggerFile' : "camera_application.log"
+        }
+    if setLogger(logInfo) == False:
+        print('Logger could not be created... Quitting')
+        return False
+    
+    envKeys = ["RTSP_STREAM_URL", "CAMERA_JWT_KEY", "API_uploadVideoURL"]
+    if verifyEnv(envKeys, logInfo) == False:
+        return False
+    
+    configInfo = {
+            'configFileName' : "config.ini"
+        }
+    if checkConfigFileExists(configInfo, folders, logInfo) == False:
+        print('Config file could not be created... Quitting')
         return False
         
     return True
